f0ae8c36820793fa0571ec466b388ec8f5df841f
KudraDube <kudradube@gmail.com>
2022-09-08 16:11:48 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * This program will print "Programming is like building a multilingual puzzle
+ * Return: 0
+ * /
+ int main(void)
+ {
+         printf("with proper grammar, but the outcoem is a piece of art,\n");
+       return (0);
+       }       
