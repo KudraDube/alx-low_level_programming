ea6d0faaea666139285ce1bb6b8519207a626415
KudraDube <kudradube@gmail.com>
2022-09-11 06:47:44 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - Prints out the last part of a quote in the standard error.
- * Return: 1
+ * Return: 1 if succcess
  */
 int main(void)
 {
-	write(2, "and that peice of art is useful" - Dora Korpar, 2015-10-19\n", 58);
-	return (1);
+write(2, "and that peice of art is useful" - Dora Korpar, 2015-10-19\n", 58);
+return (1);
 }
