6a33fe51770c306008021fa76c08efae1b3e077f
KudraDube <kudradube@gmail.com>
2022-10-04 11:28:32 -0700

    task 0

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,5 @@
-#ifndef MAIN_H 
+#ifndef MAIN_H
 #define MAIN_H
-
 void free_grid(int **grid, int height);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
@@ -9,3 +8,4 @@ char *create_array(unsigned int size, char c);
 int **alloc_grid(int width, int height);
 char *argstostr(int ac, char **av);
 #endif/*MAIN_H*/
+
