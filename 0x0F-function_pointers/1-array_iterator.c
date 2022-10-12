427c656e5705a757057615571fc9704779f2f9ae
KudraDube <kudradube@gmail.com>
2022-10-12 08:41:38 -0700

    task 0-2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,22 +1,16 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a func given as a param on each element of array
- * @array: array to execute func on
+ * array_iterator - execute a function pointer on each element of an array
+ * @array: array of integers
  * @size: size of array
- * @action: pointer to the func to use
- *
- * Return: void
+ * @action: function pointer
  */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array && action)
-	{
-		for (i = 0; i < size; i++)
-		{
-			action(array[i]);
-		}
-	}
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,27 +1,25 @@
 #include "function_pointers.h"
 
 /**
- * int_index - searches for an integer
- * @array: array to serch through
+ * int_index - Function that searches for an integer
+ * @array: array of integers
  * @size: size of array
- * @cmp: function used to compare
- *
- * Return: first index for which cmp doesn't return 0, or -1
+ * @cmp: function pointer
+ * Return: index of first element that matches with `cmp`, or -1 if none found
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, r;
+	int i;
 
-	if (size > 0 && array && cmp)
+	if (size < 1 || array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			r = cmp(array[i]);
-			if (r)
-				break;
-		}
-		if (i < size)
+		if (cmp(array[i]))
 			return (i);
 	}
+
 	return (-1);
 }
