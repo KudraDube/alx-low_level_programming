2c11edbcda5e4cf911375c321d393c921fcd0c18
KudraDube <kudradube@gmail.com>
2022-10-27 09:58:42 -0700

    mandatory tasks

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,24 +1,19 @@
-/*
- * File: 2-get_bit.c
- */
-
 #include "main.h"
-
+#include <stdio.h>
 /**
- * get_bit - Gets the value of a bit at a given index.
- * @n: The bit.
- * @index: The index to get the value at - indices start at 0.
- *
- * Return: If an error occurs - -1.
- *         Otherwise - The value of bit at index.
+ * get_bit - get the value of a bit at a given index
+ * @n: number to evaluate
+ * @index: index starting from 0, of the bit we want to get
+ * Return: Value of bit at index, or -1 if error
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
+	unsigned long int hold;
+
+	if (index > 64)
 		return (-1);
 
-	if ((n & (1 << index)) == 0)
-		return (0);
+	hold = n >> index;
 
-	return (1);
+	return (hold & 1);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,12 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/*
- * File: holberton.h
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x13-bit_manipulation directory.
- */
-
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -15,5 +8,4 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-
-#endif /*MAIN_H*/
+#endif
